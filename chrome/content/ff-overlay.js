7cc2df037a964d4417b559bfe2b3e02a12be455b
Ayke van Laethem <aykevanlaethem@gmail.com>
2011-05-17 21:04:53 +0200

    First try for a Firefox port
    This adds an extra toolbar button, just like in Chrome, but gives an
    alert ("Hello World!") when clicked, not a popup.
    
    This directory may be used for both firefox and Chrome.

---
+++ chrome/content/ff-overlay.js
@@ -0,0 +1,11 @@
+sync2all.onFirefoxLoad = function(event) {
+  document.getElementById("contentAreaContextMenu")
+          .addEventListener("popupshowing", function (e){ sync2all.showFirefoxContextMenu(e); }, false);
+};
+
+sync2all.showFirefoxContextMenu = function(event) {
+  // show or hide the menuitem based on what the context menu is on
+  document.getElementById("context-sync2all").hidden = gContextMenu.onImage;
+};
+
+window.addEventListener("load", function () { sync2all.onFirefoxLoad(); }, false);
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.sync2all.boolpref", false);
+pref("extensions.sync2all.intpref", 0);
+pref("extensions.sync2all.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.sync2all@github.com.description", "chrome://sync2all/locale/overlay.properties");
