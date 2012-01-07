57504b1181d24ef7316de99a5368323175ff2888
Ayke van Laethem <aykevanlaethem@gmail.com>
2012-01-07 16:48:27 +0100

    Got a few steps further in Firefox Sync, but I am stuck now

--- chrome/content/links/firefox-sync.js
+++ chrome/content/links/firefox-sync.js
@@ -7,17 +7,12 @@ function FirefoxSyncLink () {
 
 	this.name     = 'firefoxSync';
 	this.fullName = 'Firefox Sync';
+	this.HMAC_INPUT = "Sync-AES_256_CBC-HMAC256";
 
-	this.email    = localStorage.ffs_email;
-	this.password = localStorage.ffs_password;
+	this.email      = localStorage.ffs_email;
+	this.password   = localStorage.ffs_password;
+	this.synckey_ui = localStorage.ffs_synckey_ui;
 }
-
-FirefoxSyncLink.prototype.getPassword = function () {
-	if (this.password) return this.password;
-	this.password = prompt('Firefox Sync password:');
-	localStorage["ffs_password"] = this.password;
-	return this.password;
-};
 
 FirefoxSyncLink.prototype.getUsername = function () {
 	if (!this.username) {
@@ -28,34 +23,89 @@ FirefoxSyncLink.prototype.getUsername = function () {
 	return this.username;
 }
 
-FirefoxSyncLink.prototype.start = function () {
-
-	// log in
-
-	var xhr = new XMLHttpRequest();
-	// get the node
-	xhr.open('GET', 'https://auth.services.mozilla.com/user/1.0/'+this.getUsername()+'/node/weave', true);
-	xhr.onload = function () {
-		this.server_node_url = xhr.responseText;
-		this.getCollections();
-	}.bind(this);
-	xhr.send();
+FirefoxSyncLink.prototype.getPassword = function () {
+	if (!this.password) {
+		this.password = prompt('Firefox Sync password:');
+		localStorage["ffs_password"] = this.password;
+	}
+	return this.password;
 };
+
+FirefoxSyncLink.prototype.getSyncKey = function () {
+	if (!this.synckey) {
+		this.synckey = this.synckey_ui.replace(/-/g, '').replace(/9/g, 'o').replace(/8/g, 'l');
+	}
+	return this.synckey;
+}
+
+FirefoxSyncLink.prototype.getEncryptionKey = function () {
+	if (!this.encryptionKey) {
+		this.encryptionKey = Crypto.HMAC(Crypto.SHA256, this.getSyncKey(), this.HMAC_INPUT + this.getUsername() + "\x01")
+	}
+	return this.encryptionKey;
+}
+
+FirefoxSyncLink.prototype.getHmacKey = function () {
+	if (!this.hmacKey) {
+		this.hmacKey = Crypto.HMAC(Crypto.SHA256, this.getSyncKey(), this.getEncryptionKey() + this.HMAC_INPUT + this.getUsername() + "\x02")
+	}
+	return this.hmacKey;
+}
 
 FirefoxSyncLink.prototype.sendRequest = function (path, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', this.server_node_url+'1.0/'+this.getUsername()+path, true);
-	xhr.setRequestHeader("Authorization", "Basic "+btoa(this.getUsername+':'+this.getPassword()));
+	xhr.setRequestHeader("Authorization", "Basic "+btoa(this.getUsername()+':'+this.getPassword()));
 	xhr.onload  = function () {
-		callback(true, xhr);
+		if (xhr.status == 401) throw '401 unauthorized';
+		var data = JSON.parse(xhr.responseText);
+		// TODO decrypt if necessary
+		var payload = JSON.parse(data.payload);
+		callback(payload);
 	}.bind(this);
 	xhr.onerror = function () {
-		callback(false, xhr);
+		callback(undefined, xhr);
 	}.bind(this);
 	xhr.send();
 };
 
-FirefoxSyncLink.prototype.getCollections = function () {
+// get node, log in
+FirefoxSyncLink.prototype.connect = function (callback) {
+
+	var nodeXhr = new XMLHttpRequest();
+	// get the node
+	nodeXhr.open('GET', 'https://auth.services.mozilla.com/user/1.0/'+this.getUsername()+'/node/weave', true);
+	nodeXhr.onload = function () {
+		this.server_node_url = nodeXhr.responseText;
+		this.sendRequest('/storage/meta/global', function (metadata) {
+				// check for valid metadata
+				if (!metadata) throw 'Failed to load metadata';
+				if (metadata.storageVersion != 5) throw 'This client is getting old.';
+				this.metadata = metadata;
+				this.sendRequest('/storage/crypto/keys', function (keys) {
+						console.log(keys);
+						this.keys_result = keys;
+					}.bind(this));
+				callback();
+			}.bind(this));
+	}.bind(this);
+	nodeXhr.send();
+};
+
+FirefoxSyncLink.prototype.loadBookmarks = function (callback) {
+	// Do first error-checking
+	if (!this.metadata.engines.bookmarks)
+		throw "Can't sync bookmarks to Firefox Sync (sync Firefox first)'";
+	if (this.metadata.engines.bookmarks.version != 2)
+		throw "This client is getting old: /storage/meta/global!engines.bookmarks.version != 2";
+	// drop state if there was a very big update
+	if (localStorage.ffs_state && this.metadata.engines.bookmarks.syncID != localStorage.ffs_state_version) {
+		delete localStorage.ffs_state;
+		delete localStorage.ffs_state_version;
+	}
+}
+
+/*FirefoxSyncLink.prototype.getCollections = function () {
 	this.sendRequest ('/info/collections',
 			function (success, xhr) {
 				if (!success) {
@@ -84,7 +134,7 @@ FirefoxSyncLink.prototype.getKeys_callback = function (success, xhr) {
 	// I hope this is a bit future-proof, but I doubt it...
 	this.privkey_data = JSON.parse(keys[0].payload);
 	this.pubkey_data = JSON.parse(keys[1].payload);
-};
+};*/
 
 // test whether the engine works
 FirefoxSyncLink.prototype.test = function () {
@@ -96,5 +146,10 @@ FirefoxSyncLink.prototype.test = function () {
 
 if (debug) {
 	var ffs = new FirefoxSyncLink();
-	ffs.start();
+	ffs.connect(function () {
+			console.log('Connected with Firefox Sync');
+			ffs.loadBookmarks(function (bookmarks) {
+					console.log('Bookmarks loaded');
+				});
+		});
 }
